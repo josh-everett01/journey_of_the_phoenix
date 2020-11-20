7bf232507e0bb8628220644b5fadfd281930e3af
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2020-11-20 12:17:04 -0600

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <phoenix></phoenix>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Phoenix from './components/Phoenix.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Phoenix,
   }
 }
 </script>
